eb52e317c741a8c4cd53f772e4c8a439d366045d
oishigyunyu <yamamuuuura@gmail.com>
2021-12-20 22:13:38 +0900

    #endif is final line git add .

--- src/color.h
+++ src/color.h
@@ -6,7 +6,7 @@
 #include <iostream>
 
 void write_color(std::ostream &out, color pixel_color) {
-    // 各成分を[0, 255]に変換して出力する
+    // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
         << static_cast<int>(255.999 * pixel_color.z()) << '\n';
