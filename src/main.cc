8c7d483ac5c4163f2e5c39ac0fb14dc465944f4c
oishigyunyu <yamamuuuura@gmail.com>
2021-12-21 22:15:08 +0900

    rendering a red shpere

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,20 @@
 #include "ray.h"
 #include <iostream>
 
+
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -33,7 +46,7 @@ int main() {
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
-            
+
             write_color(std::cout, pixel_color);
         }
     }
