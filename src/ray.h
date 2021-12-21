f8e8578aa369a624e990f743c899e7b8132a77a0
oishigyunyu <yamamuuuura@gmail.com>
2021-12-21 22:23:23 +0900

    added at funciton

--- src/ray.h
+++ src/ray.h
@@ -11,6 +11,10 @@ class ray {
 
         point3 origin() const { return orig; }
         vec3 direction() const { return dir; }
+
+        point3 at(double t) const {
+            return orig + t * dir;
+        }
     
     public:
         point3 orig;
