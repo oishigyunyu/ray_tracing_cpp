b11102462587cbfa14742bb769a35aad3a951a66
oishigyunyu <yamamuuuura@gmail.com>
2021-12-22 20:04:02 +0900

    shpere class

--- src/hittable.h
+++ src/hittable.h
@@ -18,3 +18,26 @@ class hittable {
 };
 
 #endif
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere: public hittable {
+    public:
+        sphere() {}
+        sphere(point3 cen, double r) : center(cen) radius(r) {}
+
+        virtual bool hit (
+            const ray& r, double tmin, double tmax, hit_record& rec
+        ) const;
+
+    public:
+        point3 center;
+        double radius;
+};
+
+bool sphere::hit(
+    const
